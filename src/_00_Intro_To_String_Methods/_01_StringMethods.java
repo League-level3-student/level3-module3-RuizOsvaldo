df8788d13ef34b475bc043d8ba285ef04c1ae0a2
league <league@imac-21.attlocal.net>
2023-02-18 12:51:26 -0800

    s

--- src/_00_Intro_To_String_Methods/_01_StringMethods.java
+++ src/_00_Intro_To_String_Methods/_01_StringMethods.java
@@ -34,13 +34,24 @@ public class _01_StringMethods {
 
     // Given Strings s1 and s2, return the longer String
     public static String longerString(String s1, String s2) {
-        return null;
+    	if(s1.length() > s2.length()) {
+    		return s1;
+    	}else if(s1.length() < s2.length()) {
+    		return s2;
+    	}else {
+    		return "equal";
+    	}
+    	
     }
 
     // If String s contains the word "underscores", change all of the spaces
     // to underscores
     public static String formatSpaces(String s) {
-        return null;
+    	if(s.contains("underscores")) {
+    		s.replace(" ","_");
+
+    	}
+        return s;
     }
 
     // Return the name of the person whose LAST name would appear first if they
